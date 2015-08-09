beea3155f50d663645c275a38a78580de7da8d51
NancyKo <konancys@gmail.com>
2015-08-09 18:18:35 -0400

    Set values in the hash

--- lib/fake_hash.rb
+++ lib/fake_hash.rb
@@ -1,5 +1,19 @@
+require 'pry'
 class FakeHashWrapper
-
   # write code here
+attr_accessor :args
+
+  def initialize(*args)
+    @args = args
+  end
+
+  def [](key)
+
+    @args[0].each_pair { |key, value| return value }
+  end 
+
+  def []=(key, value)
+    @args << {key => value}
+  end
 
 end
