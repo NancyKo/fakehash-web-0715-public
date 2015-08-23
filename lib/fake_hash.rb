d1fed08e00fe428a936d5dc6f4ddd05cd973d7ed
NancyKo <konancys@gmail.com>
2015-08-23 18:22:12 -0400

    done

--- lib/fake_hash.rb
+++ lib/fake_hash.rb
@@ -4,16 +4,42 @@ class FakeHashWrapper
 attr_accessor :args
 
   def initialize(*args)
-    @args = args
+    @args = {}
   end
 
   def [](key)
-
-    @args[0].each_pair { |key, value| return value }
+    key = key.to_s unless key.class == String
+    @args[key] 
   end 
 
   def []=(key, value)
-    @args << {key => value}
+    key = key.to_s unless key.class == String
+    @args[key] = value
   end
 
+  def keys
+    sorted_keys = []
+    @args.keys.sort
+    @args.each do |key|
+      # binding.pry
+      sorted_keys << key[0].to_sym
+    end
+    sorted_keys.sort { |a, b| b <=> a }
+  end
+
+  def first_letter(letter)
+    matching_keys = []
+    @args.each do |key, value|
+      matching_keys << value if key[0] == letter 
+    end
+    matching_keys
+  end
+
+  def each 
+    i = 0
+    while i < @args.length
+      puts @args.values[i]
+      i += 1
+    end
+  end
 end
